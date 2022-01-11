41063b165f2a185a60ac85da381120f7546a42ee
Damion Russell <domino.russell@gmail.com>
2022-01-11 10:20:42 -0500

    modified:   index.html 	modified:   script.js 	new file:   style.css 	Images/

--- script.js
+++ script.js
@@ -2,63 +2,61 @@ let computerChoice;
 let playerChoice;
 let winValue = 0;
 let loseValue = 0;
-
+let tieValue = 0;
+let numberOfRounds = 0;
 // 0 = win, 1 = lose, 2 = tie
 let resultValue;
 
-alert("This is Rock, Paper, Scissors. You will play to 5 wins, can you win? Open up the console to find out.")
+function PlayRound(playerChoice){
+    numberOfRounds++;
+    document.getElementById('rounds').innerHTML = 'Rounds: '+numberOfRounds;
+    ComputerPlay();
+       if((playerChoice =="scissors" && computerChoice == "scissors") ||
+           (playerChoice =="rock" && computerChoice == "rock") ||
+           (playerChoice =="paper") && (computerChoice == "paper")){
+               resultValue = 2;
+       }
+       else if((playerChoice =="scissors" && computerChoice =="paper") ||    
+           (playerChoice =="rock" && computerChoice =="scissors") ||
+           (playerChoice =="paper"&&computerChoice =="rock")){
+               resultValue = 0;
+       }
+       else{
+           resultValue = 1;  
+       }
+       PlayGame(playerChoice,computerChoice);   
+}
 
-playGame();
-
- function playGame(){   
+ function PlayGame(playerChoice,computerChoice){   
+    console.log('The selected player choice is now '+playerChoice);
         if(resultValue == 0){
                 console.log(playerChoice + " beats " + computerChoice);
                 console.log("You win the round");
                 winValue++;
-                console.log("Current score is: "+winValue+" to "+loseValue); 
+                document.getElementById('wins').innerHTML = 'Wins: '+ winValue; 
             if(winValue == 5){
                     console.log("You won the game");
-                    resetGame();
+                    ResetGame();
                 }
-                playRound();
             }
         else if(resultValue == 1){
                 console.log(playerChoice + " loses to " + computerChoice);
                 console.log("You lose the round");
                 loseValue++;
-                console.log("Current score is: "+winValue+" to "+loseValue); 
+                document.getElementById('loses').innerHTML = 'Wins: '+ loseValue;
             if(loseValue == 5){
                     console.log("You lost the game");
-                    resetGame();
+                    ResetGame();
                 }
-                playRound();
             }
             else{
-                console.log("You both put " + playerChoice)
-                console.log("Current score is: "+winValue+" to "+loseValue);
-                playRound();
+                console.log("You both put " + playerChoice);
+                tieValue++;
+                document.getElementById('ties').innerHTML = 'Ties: '+tieValue;
             } 
+        return;
     }
- function playRound(){
-    playerChoice = prompt("Let's play Rock, Paper, Scissors. Whats your choice?").toLowerCase();
-    computerPlay();
-
-    if((playerChoice =="scissors" && computerChoice == "scissors") ||
-        (playerChoice =="rock" && computerChoice == "rock") ||
-        (playerChoice =="paper") && (computerChoice == "paper")){
-            resultValue = 2;
-    }
-    else if((playerChoice =="scissors" && computerChoice =="paper") ||    
-        (playerChoice =="rock" && computerChoice =="scissors") ||
-        (playerChoice =="paper"&&computerChoice =="rock")){
-            resultValue = 0;
-    }
-    else{
-        resultValue = 1;  
-    }   
-    playGame();
-}
-function computerPlay(){
+function ComputerPlay(){
     value = Math.floor(Math.random() * 3);
     switch(value){
         case 0:
@@ -72,11 +70,15 @@ function computerPlay(){
             break;
     }
 }
- function resetGame(){
-    console.log("Current score is: "+winValue+" to "+loseValue); 
+ function ResetGame(){
+    alert("Final score is: "+winValue+" to "+loseValue); 
     winValue = 0;
     loseValue = 0;
-    alert("Press okay to play again.")
-    playGame();
+    tieValue = 0;
+    numberOfRounds = 0;
+    document.getElementById('wins').innerHTML = 'Wins: '+ winValue;
+    document.getElementById('loses').innerHTML = 'Wins: '+ loseValue;
+    document.getElementById('ties').innerHTML = 'Ties: '+ tieValue;
+    document.getElementById('rounds').innerHTML = 'Rounds: '+ numberOfRounds;
+    return;
 }
-
